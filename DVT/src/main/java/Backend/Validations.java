b5dbc420af4a0071deb4b3f68d1ee22de8a1ae47
megan <megan@10.10.5.244>
2023-02-22 22:47:19 +0200

    Completed the rest of the screen Added new error messages Changed how to receive number input (spinner -> field) Added a welcome screen Completed all validations Completed all programming for this task

--- DVT/src/main/java/Backend/Validations.java
+++ DVT/src/main/java/Backend/Validations.java
@@ -10,13 +10,20 @@ package Backend;
  */
 public class Validations 
 {
+    //variables
+    private static int currentYear = 2023;
+    
+    
     //checks if something was entered into the name field
     public static boolean isNameFieldNull(String input)
     {
+        //the name field is empty
         if(input.compareTo("") == 0)
         {
             return true;
         }
+        
+        //name field is not empty
         return false;
     }
     
@@ -29,17 +36,139 @@ public class Validations
         boolean containsLowerCase = false;
         boolean containsNumber = false;
         boolean isCorrectLength = false;
+        
+        //running through each character in the text and running a series of tests on them
+        for(int i = 0; i < password.length() - 1; i++)
+        {
+            if(Character.isUpperCase(password.charAt(i)))
+            {
+                containsUpperCase = true;
+            }
+            
+            if(Character.isLowerCase(password.charAt(i)))
+            {
+                containsLowerCase = true;
+            }
+            
+            if(Character.isDigit(password.charAt(i)))
+            {
+                containsNumber = true;
+            }
+            
+            if(password.length() > 7)
+            {
+                isCorrectLength = true;
+            }
+        }
+        
+        
+        //checks if all the requirements are met
+        if(containsUpperCase && containsLowerCase && containsNumber && isCorrectLength)
+        {
+            return true;
+        }
+        return false;
     }
     
     
-    //checks if an option was selected
-    public static boolean isNoOptionChosen(boolean yes, boolean no)
+    //checks if the date entered is the applicable
+    public static boolean isDateReal(String dayStr, String month, String yearStr)
     {
-        if(yes || no)
+        //convverting the strings into an integer
+        int dayInt = Integer.parseInt(dayStr);
+        int yearInt = Integer.parseInt(yearStr);
+        
+        //makes sure the day of the date applies to all months(except february)
+        if(dayInt == 31)
+        {
+            if(month.compareTo("April")==0 || month.compareTo("June")==0 || month.compareTo("September")==0 || month.compareTo("November")==0)
+            {
+                return false;
+            }
+        }
+        
+        //running checks on February
+        if(month.equals("February"))
+        {
+            //checks if the day is the extra day in a leap year 
+            if(dayInt == 29 && yearInt%4 != 0)
+            {
+                return false;
+            }
+            
+            if(dayInt == 30 || dayInt == 31)
+            {
+                return false;
+            }
+        }
+        
+        //date exists
+        return true;
+    }
+    
+    
+    //checks if the age fits in with a range that exists
+    public static boolean isAgeWithinRange(String age)
+    {
+        //makes sure that there is something in the field
+        if(age.equals(""))
         {
             return false;
         }
         
+        if(Character.isDigit(age.charAt(0))) //will know if the age is an integer from the first character
+        {
+            //converting the string into an integer
+            int ageInt = Integer.parseInt(age);
+
+            if(ageInt < 121 && ageInt > 17 )
+            {
+                return true;
+            }
+        }
+        
+        //either the age was typed as a word or it did not fit into the range
+        return false;
+    }
+    
+    
+    //checks if the birthdate and age coincide
+    public static boolean isBirthdayAndAgeRelated(String ageStr, String yearStr)
+    {
+        //makes sure that there is something inputted for age
+        if(ageStr.equals(""))
+        {
+            return false;
+        }
+        
+        //converting strings to integers
+        int ageInt = Integer.parseInt(ageStr);
+        int yearInt = Integer.parseInt(yearStr);
+        
+        //don't coincide
+        if((currentYear - yearInt) != ageInt)
+        {
+            return false;
+        }
+        
+        //do coincide
+        return true;
+    }
+    
+    
+    //checks if an option was selected
+    public static boolean isNoOrTwoOptionsChosen(boolean yes, boolean no)
+    {
+         if(yes && no)
+        {
+            return true;
+        }
+        
+        if(yes || no)
+        {
+            return false;
+        }
+
         return true;
     }
 }
